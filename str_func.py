88bb9b58e5044afdb4af7f426f4f8ada63257c1d
Alexey <Ultrabob@inbox.ru>
2023-01-25 18:20:10 +0300

    add new func and docstrings

--- str_func.py
+++ str_func.py
@@ -1,2 +1,12 @@
 def upper_str(string):
+    """
+    Функция делает все буквы заглавными
+    """
     return string.upper()
+
+
+def title_str(string):
+    """
+    функция возвращает слова с заглавной буквы
+    """
+    return string.title()
